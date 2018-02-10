90143188f47b269a47ae9b2509c0f04498542f42
SirFurness <eric.1334@icloud.com>
2018-02-09 22:47:54 -0800

    Clean up code

--- background.js
+++ background.js
@@ -1,9 +1,8 @@
 var tabs = []
 
-chrome.tabs.on
-
 chrome.tabs.onUpdated.addListener(
     function(tabId, changeInfo, tab) {
+        //is youtube and is not a playlist
 	if(tab.url.indexOf("youtube.com") !== -1 && tab.url.indexOf("&list=") === -1) {
 	    addTabIfNeeded(tab);
 	    u2k(tab.url, tab);
@@ -11,7 +10,9 @@ chrome.tabs.onUpdated.addListener(
     }
 );
 
+//youtube to khan
 function u2k(url, currentTab) {
+    //don't redirect back to khan if this is the youtube link just redirected from
     if(currentTab.url != tabs[currentTab.id].pastUrl) {
 	chrome.tabs.update(currentTab.id, {url: "https://www.khanacademy.org/mission/algebra2"});
 	tabs[currentTab.id].pastUrl = url;
@@ -19,34 +20,27 @@ function u2k(url, currentTab) {
     }
 }
 
+//khan to youtube
 function k2u(currentTab) {
-    if(!(currentTab.id in tabs)) {
-	return;
-    }
-    if(tabs[currentTab.id].canContinue) {
+    if(currentTab.id in tabs && tabs[currentTab.id].canContinue) {
 	chrome.tabs.update(currentTab.id, {url: tabs[currentTab.id].pastUrl});
+        tabs[currentTab.id].canContinue = false;
     }
-    tabs[currentTab.id]['canContinue'] = false;
 }
 
+//add currentTab to tabs array if not already there
 function addTabIfNeeded(currentTab) {
     if(!(currentTab.id in tabs)) {
-	tabs[currentTab.id] = {tab: currentTab, id: currentTab.id, shouldChange: true, pastUrl: '', canContinue: false};
+	tabs[currentTab.id] = {pastUrl: '', canContinue: false};
     }
 }
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
 	chrome.tabs.query({active: true, currentWindow: true}, function(arrayOfTabs) {
-	    var currentTab =  arrayOfTabs[0];
-	    
-	   // if(request.message == "u2k") {
-	//	addTabIfNeeded(currentTab);
-	//	u2k(request.url, currentTab);
-
-	  //  }
+            //was continue pressed
 	    if(request.message == "k2u") {
-	        k2u(currentTab);
+	        k2u(arrayOfTabs[0]);
 	    }
 	});
 	
